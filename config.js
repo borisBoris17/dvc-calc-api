ada36b2a8a03c3b3ef9d978cf05f1aff41992917
Tucker <tuckerbichsel@tuckers-mbp.lan>
2022-09-12 22:35:55 -0500

    Dockerize app - Tucker

---
+++ config.js
@@ -0,0 +1,14 @@
+const env = process.env;
+
+const config = {
+  db: { /* do not put password or any sensitive info here, done only for demo */
+    host: env.DB_HOST || 'localhost',
+    port: env.DB_PORT || '5432',
+    user: env.DB_USER || 'tuckerbichsel',
+    password: env.DB_PASSWORD || 'password',
+    database: env.DB_NAME || 'dvc_api',
+  },
+  listPerPage: env.LIST_PER_PAGE || 10,
+};
+
+module.exports = config;
